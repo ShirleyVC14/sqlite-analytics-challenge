6505e78037fb724c66d3386b210e9826eb122cb9
Shirley <your.email@usf.edu>
2026-04-19 11:55:36 -0400

    Task 1: Top customers by total spend"

---
+++ sqlitelab.session.sql
@@ -0,0 +1,11 @@
+-- TASK 1: Top 5 Customers by Total Spend
+
+SELECT 
+    c.first_name || ' ' || c.last_name AS customer_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o ON c.id = o.customer_id
+JOIN order_items oi ON o.id = oi.order_id
+GROUP BY c.id
+ORDER BY total_spend DESC
+LIMIT 5;
